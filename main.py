3079f9a16d3b3e7c367ed8292e8c461791694ad9
mark <markpackham1@gmail.com>
2020-09-29 12:14:31 +0100

    log updated, https://youtu.be/BHwgnGEhYG8?t=1387

--- main.py
+++ main.py
@@ -7,6 +7,35 @@ def add_log(text,user):
     log_id = cursor.lastrowid
     print("Added log {}".format(log_id))
 
-add_log("This is log one", "Mark")
-add_log("This is log two", "Alice")
-add_log("This is log three", "Bob")
+# add_log("This is log one", "Mark")
+# add_log("This is log two", "Alice")
+# add_log("This is log three", "Bob")
+
+
+def get_logs():
+    sql = ("SELECT * FROM logs ORDER BY created DESC")
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row[1])
+
+def get_log(id):
+    sql = ("SELECT * FROM logs WHERE id = %s")
+    cursor.execute(sql, (id,))
+    result = cursor.fetchone()
+    print ("The specific log info for log id: {}".format(id))
+    for row in result:
+        print(row)
+
+get_log(1)
+
+
+def update_log(id, text):
+    sql = ("UPDATE logs SET text = %s WHERE id = %s")
+    cursor.execute(sql, (text, id))
+    db.commit()
+    print("Log {} updated".format(id))
+
+update_log(2, 'I have been UPDATED!!!')
+get_logs()
